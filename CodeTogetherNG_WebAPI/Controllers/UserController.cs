11b33e493bece653b2de1862b167bbd8dfeee85e
Pisanina <mailszarookiego@o2.pl>
2019-04-05 11:07:29 +0200

    Task137: WebAPI - get list of members+ tests

--- CodeTogetherNG_WebAPI/Controllers/UserController.cs
+++ CodeTogetherNG_WebAPI/Controllers/UserController.cs
@@ -1,11 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using CodeTogetherNG_WebAPI.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using CodeTogetherNG_WebAPI.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace CodeTogetherNG_WebAPI.Controllers
 {
@@ -20,11 +20,9 @@ namespace CodeTogetherNG_WebAPI.Controllers
             _context = context;
         }
 
-       
-        [HttpGet]
+        [HttpGet("{userId}")]
         public JsonResult Profile(string userId)
         {
-
             var userSkills = _context.UserTechnologyLevel.Where(u => u.UserId == userId)
                 .Select(t => new { t.Technology.TechName, t.TechLevel });
 
@@ -33,9 +31,22 @@ namespace CodeTogetherNG_WebAPI.Controllers
             var userMember = _context.ProjectMember.Where(m => (m.MemberId == userId)
                                 && (m.AddMember == true)).Select(u => new {u.Project.Id, u.Project.Title });
 
-
             return new JsonResult(new { userSkills, userOwner, userMember });
+        }
 
+        [HttpGet]
+        public JsonResult Users()
+        {
+            return new JsonResult(_context.AspNetUsers.Select(u => new
+            {
+                u.Id,
+                u.UserName,
+                Owner = u.Project.Select(m => new { m.OwnerId }).Count(),
+                Member = u.ProjectMember.Select(m => new { m.MemberId }).Count(),
+                Beginner = u.UserTechnologyLevel.Where(t =>  t.TechLevel==1).Count(),
+                Advanced = u.UserTechnologyLevel.Where(t =>  t.TechLevel==2).Count(),
+                Expert = u.UserTechnologyLevel.Where(t =>  t.TechLevel==3).Count()
+            }));
         }
     }
 }
